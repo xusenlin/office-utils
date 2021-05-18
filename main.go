8091c330ed5085c350d50b76118cc86f79a888c2
五木老祖 <wumulaozu@gmail.com>
2021-05-18 15:35:41 +0800

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 func Cors(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")  // 允许访问所有域，可以换成具体url，注意仅具体url才能带cookie信息
-		w.Header().Add("Access-Control-Allow-Headers", "*") //header的类型
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type,Authorization") //header的类型
 		w.Header().Add("Access-Control-Allow-Credentials", "true") //设置为true，允许ajax异步请求带cookie信息
 		w.Header().Add("Access-Control-Allow-Methods", "*") //允许请求方法
 
